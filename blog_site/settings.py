8c5067f5a17d97663b8bbc0a21c8e9241527906d
Gabriel Reyes <gabri.rys@gmail.com>
2023-08-02 17:38:21 -0400

    Migraciones iniciales -creación de base de datos-

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "app_blog",
+    "app_contacto",
+    "app_perfiles",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,11 @@ ROOT_URLCONF = 'blog_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [#AGREGAR TAMPLATES DE APPS EN ESTE FORNATO
+            #BASE_DIR / "templates_base/",
+            #BASE_DIR / "perfiles/templates/",
+            #BASE_DIR / "control_estudio/templates/",
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
