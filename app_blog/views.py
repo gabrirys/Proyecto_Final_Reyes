b9695e3193c16065f85c06b4202b7796bc7aec16
Gabriel Reyes <gabri.rys@gmail.com>
2023-08-05 18:47:43 -0400

    Agregado de view crear_articulo

--- app_blog/views.py
+++ app_blog/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from .forms import ArticuloFormulario
 
-# Create your views here.
+
+def crear_articulo(request):
+    if request.method == "POST":
+        formulario = ArticuloFormulario(request.POST, request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            url_exitosa = reverse('articulo_exitoso')
+            return redirect(url_exitosa)
+    else:  # GET
+        formulario = ArticuloFormulario()
+        http_response = render(
+        request=request,
+        template_name='crear_articulo.html',
+        context={'formulario': formulario}
+    )
+    return http_response
+            
