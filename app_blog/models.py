d313009e30977a73452e328f31b4f602e4f3a53a
Gabriel Reyes <gabri.rys@gmail.com>
2023-08-05 09:30:54 -0400

    App_blog - Creación de modelo Articulo

--- app_blog/models.py
+++ app_blog/models.py
@@ -1,4 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+class Articulo(models.Model):
+    titulo = models.CharField(max_length=128)
+    subtitulo = models.CharField(max_length=128)
+    contenido = models.TextField(blank=True)
+    fecha_publicado = models.DateTimeField(auto_now_add=True)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='articulos_creados', null=True)
+    imagen = models.ImageField(upload_to='avatares', null=True, blank=True) # imagen
+    
+    
+    def __str__(self):
+        return f"{self.titulo} - {self.autor} -" 
--- app_perfiles/urls.py
+++ app_perfiles/urls.py
@@ -7,8 +7,8 @@ registro, login_view, CustomLogoutView, MiPerfilUpdateView, # agregar_avatar
 
 urlpatterns = [
     path("registro/", registro, name="registro"),
-    path("login/", login_view, name="login"),
-    path('logout/', CustomLogoutView.as_view(), name="logout"),
-    path('editar-mi-perfil/', MiPerfilUpdateView.as_view(), name="editar_perfil"),
+    #path("login/", login_view, name="login"),
+    #path('logout/', CustomLogoutView.as_view(), name="logout"),
+    #path('editar-mi-perfil/', MiPerfilUpdateView.as_view(), name="editar_perfil"),
     # path('agregar-avatar/', agregar_avatar, name="agregar_avatar"),
 ]
