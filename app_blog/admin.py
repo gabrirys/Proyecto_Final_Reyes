824242be6159b74cd31bdcf24ba20f08e0b22c2d
Gabriel Reyes <gabri.rys@gmail.com>
2023-08-12 13:28:13 -0400

    Personalización de panel administrativo Articulo

--- app_blog/admin.py
+++ app_blog/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from app_blog.models import Articulo
 
-admin.site.register(Articulo)
+#admin.site.register(Articulo)
+@admin.register(Articulo)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ("titulo", "autor")
+    list_filter = ("autor",)
+    list_per_page = 6
+    search_fields = ("titulo", "autor")
+    
