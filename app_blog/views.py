a2a77eb65fd414f3cc5a750ac152409d93601a04
Gabriel Reyes <gabri.rys@gmail.com>
2023-08-12 17:43:29 -0400

    Agregado de mixin en Blog

--- app_blog/views.py
+++ app_blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse, reverse_lazy
 from .models import Articulo
 from .forms import ArticuloFormulario
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 
@@ -12,25 +13,34 @@ class ArticuloListView(ListView):
     template_name = 'articulo_lista.html'
     context_object_name = 'articulos'
 
+
 class ArticuloDetailView(DetailView):
     model = Articulo
     template_name = 'app_blog/articulo_detail.html'
     context_object_name = 'articulo'
 
-class ArticuloCreateView(CreateView):
+
+class ArticuloCreateView(LoginRequiredMixin, CreateView):
     model = Articulo
     form_class = ArticuloFormulario
     template_name = 'app_blog/articulo_formulario.html'
-    success_url = reverse_lazy('articulo_detail')
+    
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.autor = self.request.user  # Asignar el autor al usuario actual
+        self.object.save()
+        return redirect(reverse('articulo_detail', args=[self.object.pk]))
 
-class ArticuloUpdateView(UpdateView):
+
+class ArticuloUpdateView(LoginRequiredMixin, UpdateView):
     model = Articulo
     form_class = ArticuloFormulario
     template_name = 'articulo_formulario.html'
     context_object_name = 'articulo'
     success_url = reverse_lazy('articulo_lista')
 
-class ArticuloDeleteView(DeleteView):
+
+class ArticuloDeleteView(LoginRequiredMixin, DeleteView):
     model = Articulo
     template_name = 'articulo_confirm_delete.html'
     context_object_name = 'articulo'
