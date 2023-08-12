c824d5e11a60ba46d58ba6cee32f9623b6386cbe
Gabriel Reyes <gabri.rys@gmail.com>
2023-08-12 18:55:05 -0400

    Ajuste de view, urls y templates de Contacto

--- app_contacto/urls.py
+++ app_contacto/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import contactar
+from .views import contactar, contacto_exitoso
 
 urlpatterns = [
     path('contacto/', contactar, name='contactar'),
+    path('contacto-exitoso/', contacto_exitoso, name='contacto_exitoso'),  
 ]
--- app_contacto/views.py
+++ app_contacto/views.py
@@ -1,29 +1,42 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.core.mail import send_mail # para que envíe un mail con la info del contacto
 from app_contacto.models import Contacto
 from app_contacto.forms import ContactoFormulario
 
-def contactar(request):
-   if request.method == "POST":
-       formulario = ContactoFormulario(request.POST)
-       if formulario.is_valid():
-           data = formulario.cleaned_data  # es un diccionario
-           nombre = data["nombre"]
-           email = data["email"]
-           telefono = data["telefono"]
-           mensaje = data["mensaje"]
-           
-           contacto = Contacto(nombre=nombre, email=email, telefono=telefono, mensaje=mensaje)  # lo crean solo en RAM
-           contacto.save()  # Lo guardan en la Base de datos
 
-           # Redirecciono al usuario a la página inicio
-           url_exitosa = reverse('inicio')  # me regresa a inicio
-           return redirect(url_exitosa)
-   else:  # GET
-       formulario = ContactoFormulario()
-   http_response = render(
-       request=request,
-       template_name='app_contacto/contacto.html',
-       context={'formulario': formulario}
-   )
-   return http_response
+def contactar(request):
+    if request.method == "POST":
+        formulario = ContactoFormulario(request.POST)
+        if formulario.is_valid():
+            data = formulario.cleaned_data  # es un diccionario
+            nombre = data["nombre"]
+            email = data["email"]
+            telefono = data["telefono"]
+            mensaje = data["mensaje"]
+           
+            contacto = Contacto(nombre=nombre, email=email, telefono=telefono, mensaje=mensaje)  # lo crean solo en RAM
+            contacto.save()  # Lo guardan en la Base de datos
+           
+           #(Envío de correo electrónico
+            # subject = 'Nuevo mensaje de contacto'
+            # message = f'Nombre: {nombre}\nEmail: {email}\nTeléfono: {telefono}\nMensaje: {mensaje}'
+            # from_email = 'tu@email.com'
+            # to_email = ['destinatario@email.com']
+            # send_mail(subject, message, from_email, to_email, fail_silently=False)
+
+           # Redirecciono a una página con mensaje de éxito
+            url_exitosa = reverse('contacto_exitoso')
+            return redirect(url_exitosa)
+           
+    else:  # GET
+        formulario = ContactoFormulario()
+        http_response = render(
+        request=request,
+        template_name='app_contacto/contacto.html',
+        context={'formulario': formulario}
+    )
+    return http_response
+
+def contacto_exitoso(request):
+    return render(request, 'app_contacto/contacto_exitoso.html')
