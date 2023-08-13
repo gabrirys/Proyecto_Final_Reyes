ac43cce09a56854ddc78b70345b4c46afaf0ad69
Gabriel Reyes <gabri.rys@gmail.com>
2023-08-13 16:09:30 -0400

    Corrección de template articulo_detail

--- app_blog/admin.py
+++ app_blog/admin.py
@@ -4,7 +4,7 @@ from app_blog.models import Articulo
 #admin.site.register(Articulo)
 @admin.register(Articulo)
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ("titulo", "autor")
+    list_display = ("titulo", "autor", "fecha_publicado")
     list_filter = ("autor",)
     list_per_page = 6
     search_fields = ("titulo", "autor")
