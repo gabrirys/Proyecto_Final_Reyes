979122173a220180f14c480c0a7994e20f16df8f
Gabriel Reyes <gabri.rys@gmail.com>
2023-08-15 16:07:33 -0400

    AJustes de urls y templates

--- app_chat/urls.py
+++ app_chat/urls.py
@@ -5,5 +5,7 @@ from .views import (ChatListView, ChatDetailView, MessageCreateView)
 urlpatterns = [
     path('', ChatListView.as_view(), name='chat_list'),
     path('chat/<int:pk>/', ChatDetailView.as_view(), name= 'chat_detail'),
-    path('crear-mensaje/<int:chat_id>/', MessageCreateView.as_view(), name='crear_mensaje'),  
+    path('crear-mensaje/<int:chat_id>/', MessageCreateView.as_view(), name='crear_mensaje'),
 ]
+
+  
--- app_chat/views.py
+++ app_chat/views.py
@@ -8,20 +8,20 @@ from django.utils.decorators import method_decorator
 @method_decorator(login_required, name='dispatch')
 class ChatListView(ListView):
     model = Chat
-    template_name = 'chat/chat_list.html'
+    template_name = 'app_chat/chat_list.html'
     context_object_name = 'chats'
 
 @method_decorator(login_required, name='dispatch')
 class ChatDetailView(DetailView):
     model = Chat
-    template_name = 'chat/chat_detail.html'
+    template_name = 'app_chat/chat_detail.html'
     context_object_name = 'chat'
 
 @method_decorator(login_required, name='dispatch')
 class MessageCreateView(CreateView):
     model = Message
     fields = ['texto']
-    template_name = 'chat/message_create.html'
+    template_name = 'app_chat/crear_mensaje.html'
 
     def form_valid(self, form):
         chat_id = self.kwargs['chat_id']
