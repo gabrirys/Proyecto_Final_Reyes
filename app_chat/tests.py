c836a2c1ef521165b6d115df9590fe45627cdb53
Gabriel Reyes <gabri.rys@gmail.com>
2023-08-16 18:02:07 -0400

    Creación de tests para comprobar modelos

--- app_chat/tests.py
+++ app_chat/tests.py
@@ -1,25 +1,30 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .models import Chat, Message
-from django.urls import reverse
+from .models import Thread, Message
 
-class ChatTests(TestCase):
-# Crear dos usarios para utilizar en el test
+# TEST PARA:
+# Crear hilos
+# Asignarles usuarios
+# Recuperar hilos a partir de sus usuarios
+# Crear mensajes, asignarlos a hilos y recuperarlos
+
+
+# Crear crear los usuarios y el hilo
+class ThreadTestCase(TestCase):
     def setUp(self):
-        self.usuario_1 = User.objects.create_user(username='usuario1', password='password123')
-        self.usuario_2 = User.objects.create_user(username='usuario2', password='password456')
-        self.chat = Chat.objects.create(usuario_1=self.usuario_1, usuario_2=self.usuario_2)
+        self.u1 = User.objects.create_user('u1', None, '1234')
+        self.u2 = User.objects.create_user('u2', None, '1234')
+        
+        self.thread = Thread.objects.create()
+        self.thread.user.add(self.u1, self.u2)
     
-# Comprobar la creración de mensajes
-    def test_creacion_mensaje(self):
-        mensaje = Message.objects.create(chat=self.chat, texto='Hola, usuario 2!')
-        self.assertEqual(mensaje.chat, self.chat)
-        self.assertEqual(mensaje.texto, 'Hola, usuario 2!')
-  
-# Comprobar el dialogo entre usuarios
-    def test_dialogo_entre_usuarios(self):
-        mensaje_1 = Message.objects.create(chat=self.chat, texto='Hola, usuario 2!')
-        mensaje_2 = Message.objects.create(chat=self.chat, texto='Hola, usuario 1!')
-        self.assertEqual(self.chat.message_set.count(), 2)
-        self.assertIn(mensaje_1, self.chat.message_set.all())
-        self.assertIn(mensaje_2, self.chat.message_set.all())
+    def test_add_user_to_thread(self): # Comprobar si el hilo tiene dos usuarios
+        self.thread.user.add(self.u1, self.u2)
+        self.assertEqual(len(self.thread.user.all()), 2) 
+        
+    def test_filter_threads_by_user(self): # Recuperar un hilo ya existente a partir de sus usuarios
+        self.thread.user.add(self.u1, self.u2)
+        threads = Thread.objects.filter(user=self.u1).filter(user=self.u2)
+        self.assertEqual(self.thread, threads[0])
+        
+        
