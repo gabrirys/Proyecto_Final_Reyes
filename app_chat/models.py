4a0633e0186514882ba0a0db375206fdb9a8af73
Gabriel Reyes <gabri.rys@gmail.com>
2023-08-16 17:20:18 -0400

    (Empezamos de nuevo) Modelos app_chat

--- app_chat/models.py
+++ app_chat/models.py
@@ -2,11 +2,14 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
-class Chat(models.Model):
-    usuario_1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='chats_1')
-    usuario_2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='chats_2')
-
-class Message(models.Model):
-    fecha_creacion = models.DateTimeField(auto_now_add=True)
-    texto = models.TextField()
-    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+class Message(models.Model): # Mensaje
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['created']
+        
+class Thread(models.Model): # Hilo
+    user = models.ManyToManyField(User, related_name='threads')
+    message = models.ManyToManyField(Message)
