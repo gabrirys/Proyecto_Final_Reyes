f0b99bca64497b80f33b4e5264a23e9e7d507360
Gabriel Reyes <gabri.rys@gmail.com>
2023-08-16 21:23:14 -0400

    Nuevas migraciones creación de class Mensaje

--- app_chat/models.py
+++ app_chat/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from django.conf import settings
 import uuid
+
+User = settings.AUTH_USER_MODEL 
+
 class ModelBase(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, db_index=True, editable=False)
     tiempo = models.DateTimeField(auto_now_add=True)
@@ -7,3 +11,8 @@ class ModelBase(models.Model):
     
     class Meta:
         abstract = True
+        
+        
+class Mensaje(ModelBase):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    texto = models.TextField()
